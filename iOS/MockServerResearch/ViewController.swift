0571cf282a3f2aa5e1a18b7b842f92a0b8e54f95
HungTheWizard <hung@zappasoft.com>
2017-05-13 18:31:39 +1000

    Make the iOS app connect, get, and handle Json response from the mock server.

--- iOS/MockServerResearch/ViewController.swift
+++ iOS/MockServerResearch/ViewController.swift
@@ -15,7 +15,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
+        Alamofire.request("http://127.0.0.1:5000/hello").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
